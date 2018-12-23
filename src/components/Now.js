544fd71d6cd536329a13018a982b90be0afd53cc
David Beauchamp <david123beauchamp@gmail.com>
2018-12-22 19:55:31 -0500

    Adding basic nav

--- src/components/Now.js
+++ src/components/Now.js
@@ -5,8 +5,8 @@ import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './now.css';
 
-import marker from 'leaflet/dist/images/marker-icon.png';
-import marker2x from 'leaflet/dist/images/marker-icon-2x.png';
+import marker from '../img/map-marker.png';
+import marker2x from '../img/map-marker-x2.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 delete L.Icon.Default.prototype._getIconUrl;
@@ -14,23 +14,27 @@ delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: marker2x,
   iconUrl: marker,
-  shadowUrl: markerShadow
+  shadowUrl: markerShadow,
+  iconSize: [50, 50],
+  iconAnchor: [25, 50],
+  shadowSize: [100, 100],
+  shadowAnchor: [28, 100],
+  popupAnchor: [0, -50],
 });
 
 export default class WhereAmINowMap extends Component {
   state = {
     lat: 6.244203,
     lng: -75.581215,
-    zoom: 5,
+    zoom: 3,
   }
 
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map center={position} zoom={this.state.zoom} style={{ width: '100vw', height: '100vh' }}>
+      <Map center={position} zoom={this.state.zoom} style={{ width: '100vw', height: 'calc(100vh - 50px)' }}>
         <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url='https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
         />
         <Marker position={position}>
           <Popup>Medellin, Colombia</Popup>
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import './nav.css';
 
 export default class Nav extends Component {
   render() {
     return (
 			<nav>
-			
+				<ul className={'nav-ul'}>
+					<li>
+						<a href="#">Where am I now</a>
+					</li>
+					<li>
+						<img className={'nav-img'} src="http://travel.daveb.co/wp-content/themes/travelblog/images/profile-photo.jpg" alt="Picture of me" />
+					</li>
+					<li>
+						<a href="#">Places Ive been</a>
+					</li>
+				</ul>
 			</nav>
 		)
 	}
